acc90a2a0b5501ed848aec79015845b697fb307d
brenermr <brenercaique0806@gmail.com>
2021-12-05 13:36:41 -0300

    goFinances

--- src/screens/Dashboard/style.ts
+++ src/screens/Dashboard/style.ts
@@ -1,10 +1,10 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex:1;
     align-items: center;
     justify-content: center;
-    background-color: white;
+    background-color: ${({theme})=>theme.colors.primary};
 `;
 
 export const Title = styled.Text`
