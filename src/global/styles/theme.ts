951ab2199a177cb79e9d551c3ba8760a19838cdc
brenermr <brenercaique0806@gmail.com>
2021-12-05 12:57:20 -0300

    goFinances

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export default {
+    colors : {
+        blue:'#5636D3',
+        orange:'#FF872C',
+        green:'#12A454',
+        red:'#E83F5B',
+        shape:'#FFFFF',
+        title:'#363F5F',
+        text:'#969CB2',
+        background:'#F0F2F5'
+    }
+}
