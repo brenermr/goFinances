a1d65ad7a29bf2456b4ef7a39c588f479a22e193
brenermr <brenercaique0806@gmail.com>
2021-12-05 20:59:11 -0300

    goFinances

--- src/global/styles/styled.d.ts
+++ src/global/styles/styled.d.ts
@@ -5,7 +5,3 @@ declare module 'styled-components'{
     type ThemeType = typeof theme
     export interface DefaultTheme extends ThemeType{}
 }
-
-import 'styled-components'
-
-import theme from './theme'
